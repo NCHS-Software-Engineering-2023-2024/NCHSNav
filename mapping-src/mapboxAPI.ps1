2ccb04912eeb0f189b94439ba6e2480cbf3e388d
evan203 <evancreeper203@gmail.com>
2024-01-31 14:22:41 -0600

    mapbox sdk or something

---
+++ mapping-src/mapboxAPI.ps1
@@ -0,0 +1,4 @@
+$API_TOKEN = "pk.eyJ1IjoiZWpzY2FtZWhvcm4iLCJhIjoiY2xycGNxOHp0MDRhZTJqbmozM3NzZG8zMyJ9.7J-8aqTiiF5kSpYyEnj2qQ"
+
+
+Invoke-WebRequest "https://api.mapbox.com/datasets/v1/ejscamehorn?access_token=pk.eyJ1IjoiZWpzY2FtZWhvcm4iLCJhIjoiY2xycGNxOHp0MDRhZTJqbmozM3NzZG8zMyJ9.7J-8aqTiiF5kSpYyEnj2qQ"
