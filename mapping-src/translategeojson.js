b9d7c5d4f6cf196b147edab4a3598f1ac1e53bb6
evan203 <evancreeper203@gmail.com>
2024-01-30 23:30:01 -0600

    georeference NCHS Map pdf in qgis

--- mapping-src/translategeojson.js
+++ mapping-src/translategeojson.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+var transformTranslate = require('@turf/transform-translate');
 
 // Specify the path to your GeoJSON file
 const filePath = 'projected map.geojson';
+const outputFilePath = 'translated map.geojson';
 
 // Read the GeoJSON file
 fs.readFile(filePath, 'utf8', (err, data) => {
@@ -16,6 +18,15 @@ fs.readFile(filePath, 'utf8', (err, data) => {
 
     // Now you can use the geojsonData object in your code
     // https://turfjs.org/docs/#transformTranslate
+    var translatedGeoJson = transformTranslate(geojsonData, 8000, 270);
+    // Write the translated GeoJSON to the output file
+    fs.writeFile(outputFilePath, JSON.stringify(translatedGeoJson), 'utf8', (writeErr) => {
+      if (writeErr) {
+        console.error('Error writing file:', writeErr);
+        return;
+      }
+      console.log('Translation complete. Check the translated map at:', outputFilePath);
+    });
   } catch (parseError) {
     console.error('Error parsing GeoJSON:', parseError);
   }
