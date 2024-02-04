391f5775a531ffe681f55f15ed6031b4703a4114
evan203 <evancreeper203@gmail.com>
2024-02-04 17:59:56 -0600

    type saftey (?)

--- app/map.tsx
+++ app/map.tsx
@@ -4,38 +4,42 @@
 
 // react mapbox gl library
 import 'mapbox-gl/dist/mapbox-gl.css';
-import Map, {Source, Layer} from "react-map-gl"
+import Map, {Source, Layer, GeolocateControl, ScaleControl, NavigationControl } from "react-map-gl"
+
+const floorplan: any = {
+  id: "floor_plan",
+  type: "line",
+  paint: {
+    'fill-color': 'black',
+    'fill-opacity': 0.9
+  }
+}
+
+const mapAttr: any = {
+  mapboxAccessToken: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
+  initialViewState: {
+      longitude: -88.1559,
+      latitude: 41.7674,
+      zoom: 17
+    },
+    mapStyle: "mapbox://styles/ejscamehorn/cls6whfn702cy01p1gmy2geqo",
+    width: "100%",
+    height: "100%", 
+    style: {
+      position: 'absolute',
+      top: 0,
+      left: 0
+    }
+}
 
 export default function SchoolMap({ data }) {
-  return (
-    <main className="h-max w-full">
-      <Map
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-        initialViewState={{
-          longitude: -88.1559,
-          latitude: 41.7674,
-          zoom: 17
-        }}
-        width="100%"
-        height="100%"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0
-          }}
-        mapStyle="mapbox://styles/ejscamehorn/cls6whfn702cy01p1gmy2geqo"
-
-      >
-        <Source type="geojson" data={data}>
-          <Layer
-            type="line"
-            paint={{
-              'fill-color': 'black',
-              'fill-opacity': 0.5
-            }}
-          />
-        </Source>
-      </Map>
-    </main>
-  )
+  return <Map {...mapAttr}  >
+    <GeolocateControl />
+    <ScaleControl />
+    <NavigationControl />
+    <Source type="geojson" data={data}>
+      <Layer {...floorplan} />
+    </Source>
+  </Map>;
 }
+
