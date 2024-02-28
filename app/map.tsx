2aef2afaf9189aea2745e9c0ebefe1679bc3c700
evan203 <evancreeper203@gmail.com>
2024-02-28 14:07:06 -0600

    remove accidental div

--- app/map.tsx
+++ app/map.tsx
@@ -70,7 +70,6 @@ export default function SchoolMap( { data , data2 } : { data: any, data2: any } 
       <Layer {...floorplan} />
     </Source>
   </Map>
-  <div>className="absolute top-4 left-4"</div>
   <div style={{ position: 'absolute', top: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1 }}>
   <input type="text" placeholder="Search Classroom" value={classroom} onChange={handleInputChange} className="p-2 rounded-lg border border-gray-300 focus:outline-none" />
   <Link href="/api/auth/signin" className="p-2 rounded-lg border border-gray-300 focus:outline-none">Sign in </Link>
