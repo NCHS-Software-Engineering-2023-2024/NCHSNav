496eb459c1ba8a251eaa87846f81a50df7cfcd98
evan203 <evancreeper203@gmail.com>
2024-03-21 14:04:05 -0500

    prepare to seed database

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+import { join } from '@prisma/client/runtime/library';
+const prisma = new PrismaClient()
+async function main() {
+    const accountSeedFile = join(process.cwd(), './prisma/seeds/accounts.csv');
+    const accountSeedPromise = prisma.$executeRaw`
+    COPY "accounts"(id, name, bank)
+    FROM ${accountSeedFile}
+    DELIMITER ','
+    CSV HEADER;
+    `;
+
+    const [accountsResult] = await Promise.all([accountSeedPromise]);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
