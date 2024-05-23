5f155c671ebdf3201ece2006a4f50f047f03ffcc
Anay Apte <aaapte@stu.naperville203.org>
2024-05-22 21:44:28 -0500

    I added the social media cards

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,34 @@ export const metadata: Metadata = {
     description: "Find your way around Central",
     siteName: "NCHS Nav"
   }
+
+  /*
+  twitter: {
+    card: 'summary_large_image',
+    site: *placeholder since we don't have a twitter*,
+    title: "NCHSNav",
+    description: "Twitter card for NCHSNav.",
+    creator: *placeholder since we don't have a twitter*,
+    images: {
+      url: *placeholder since we don't have a link*,
+      alt: 'Preview image for NCHSNav',
+    }
+  },
+  */
+
+  /*
+  facebook: {
+    card: 'summary_large_image',
+    site: *placeholder since we don't have a Facebook*,
+    title: "NCHSNav",
+    description: "Facebook card for NCHSNav.",
+    creator: *placeholder since we don't have a Facebook*,
+    images: {
+      url: *placeholder since we don't have a link*,
+      alt: 'Preview image for NCHSNav',
+    }
+  },
+  */
 };
 
 export default function RootLayout({
